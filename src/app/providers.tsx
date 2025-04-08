92114750b3887d2f9f8d77a3db369db896e6e369
Palm_Porndittha <porndittha.kh@abbon.co.th>
2025-04-08 19:23:20 +0700

    defaultTheme

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -24,7 +24,7 @@ export const Providers = ({ children, themeOptions }: ProvidersProps) => {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       enableSystem
       disableTransitionOnChange
       {...themeOptions}
